32736879b2be410204c97cc2fc10fff82871d663
Cemil Okay <cemilokay@gmail.com>
2021-01-02 21:03:41 +0000

    Add constructor with a state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Guest'
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello Guest</h1>
+        <h1>Hello {this.state.userName}</h1>
         <label htmlFor="user_name">Enter your name:</label> <br/>
         <input id="user_name" type="text"/>
       </div>
