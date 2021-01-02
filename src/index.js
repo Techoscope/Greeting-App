8e65d9b38831521610f6c63746958fa2d963df7a
Cemil Okay <cemilokay@gmail.com>
2021-01-02 20:16:57 +0000

    Use function to render

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// JSX - Rendering JSX as a first argument of ReactDOM.render()
-// Assign JSX to a variable to pass as a first argument of ReactDOM.render()
-const header = (
-  <div>
-    <h1>Hello World</h1>
-    <p>This is a parent / child & sibling exercise</p>
-  </div>
-)
+function header() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a parent / child & sibling exercise</p>
+    </div>
+  )
+} 
 
-ReactDOM.render(header,document.getElementById('root'));
+ReactDOM.render(header(),document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
