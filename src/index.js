14e6ea97fd10850552dce88ce10d87dec0c6cbd1
Cemil Okay <cemilokay@gmail.com>
2021-01-02 20:14:40 +0000

    Use variable to render

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 // JSX - Rendering JSX as a first argument of ReactDOM.render()
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+// Assign JSX to a variable to pass as a first argument of ReactDOM.render()
+const header = (
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a parent / child & sibling exercise</p>
+  </div>
+)
+
+ReactDOM.render(header,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
