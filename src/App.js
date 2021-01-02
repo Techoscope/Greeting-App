0cf772da6a19621d719fc8c17f14b54bc20583a3
Cemil Okay <cemilokay@gmail.com>
2021-01-02 20:48:21 +0000

    Convert functional component to a class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello Guest</h1>
-      <label htmlFor="user_name">Enter your name:</label> <br/>
-      <input id="user_name" type="text"/>
-    </div>
-  );
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Guest</h1>
+        <label htmlFor="user_name">Enter your name:</label> <br/>
+        <input id="user_name" type="text"/>
+      </div>
+    );
+  }
 }
 
 export default App;
